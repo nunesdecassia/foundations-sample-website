c4b28160ee21588d2375fd62d9202bb4928d436b
Cássia Nunes <cassia.nunes@code.berlin>
2021-03-06 17:12:37 +0100

    update log path to tmp folder

--- color_check/controllers/log.py
+++ color_check/controllers/log.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 from datetime import datetime
 
+Path("tmp/").mkdir(parents=True, exist_ok=True)
+
 def log(message):
 	now = datetime.now()
 	dt_string = now.strftime('%Y-%m-%d %H:%M:%S')
 	
-	final_message = '{time} - {msg}'.format(time=dt_string, msg=message) 
+	final_message = f'{dt_string} - {message}' 
 	
 	print(final_message)
 
-	with open('log.txt', 'a+') as file:
+	with open('tmp/log.txt', 'a+') as file:
 		file.write(final_message + '\n')
